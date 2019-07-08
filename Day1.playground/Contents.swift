19a507460f228639da0fe871445743ea3db6cc59
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-08 14:54:28 -0700

    first save

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 15
+"I am \(age) years old"
+
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+print (welcome)
+var first = "Karlie"
+var last = "Kloss"
+print ("\(first)\(last)")
+print ("\(last)\(first)")
+print ("\(first) \(last)")
+print ("\(last) \(first)")
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+print ((a+b+c+d)/4)
+var integer = 10.25
+var double = 20
+
+print
+
+5 < 3
+12 > 7
+6 != 8
+7 == 7
+"karlie" == "karlie"
+"karlie" == " karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+var dogAge = 1
+if  dogAge < 2 {
+    print("You are a puppy")
+}
+else if dogAge > 12 {
+    print("You are elderly")
+}
+else {
+    print("You are also awesome")
+}
+var favoriteFood = "pasta"
+if favoriteFood > "Chipotle" {
+}
+else if "pasta" > "Starbucks" {
+}
+
+var hasAccount = true
+"Please log in"
+var gradelevel = 6
+if gradelevel < 6 {
+print("elementary")
+}
+else if gradelevel > 6 {
+    print("highschool")
+}
